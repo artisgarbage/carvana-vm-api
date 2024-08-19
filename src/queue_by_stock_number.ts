85921b7739f919ee032fdee4e84f8e4f25d8523f
Anthony Chavez <artisgarbage@gmail.com>
2024-08-19 11:46:06 -0600

    Add Queue by Stock Number script

---
+++ src/queue_by_stock_number.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+// Events
+
+let config = {
+  method: "get",
+  maxBodyLength: Infinity,
+  url: "",
+};
+
+export function queue(attemptRealVend: Boolean, stockNumber: String) {
+  if (attemptRealVend) {
+    console.log("🚨🚨🪙 Attempt to Queue a Real Car 🪙🚨🚨");
+    config.url = `http://10.51.205.253/exe.cgi?cmd=set%20requestID%20%${stockNumber}%22`;
+    axios
+      .request(config)
+      .then((response) => {
+        let responseData = JSON.stringify(response.data);
+        console.log(responseData);
+        return responseData;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    console.log("Use test Stock Number TestingTesting123");
+    stockNumber = "TestingTesting123";
+    config.url = `http://10.51.205.253/exe.cgi?cmd=set%20requestID%20%${stockNumber}%22`;
+    axios
+      .request(config)
+      .then((response) => {
+        let responseData = JSON.stringify(response.data);
+        console.log(responseData);
+        return responseData;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
